8bbffb2122e862dd96ce025950f841cd7f75b99e
Basil Alasadi <8195420+generic19@users.noreply.github.com>
2025-05-29 01:19:14 +0300

    Project Skeleton. Installed Swift packages:
    - Alamofire
    - Firebase (core, analytics, auth, firestore)
    - Swinject
    - MobileBuySDK

---
+++ Shopped-In/Presentation/App/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Shopped-In
+//
+//  Created by Basel Alasadi on 29/05/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shopped-In/Secrets.swift
@@ -0,0 +1,7 @@
+//
+//  Secrets.swift
+//  Shopped-In
+//
+//  Created by Basel Alasadi on 29/05/2025.
+//
+
